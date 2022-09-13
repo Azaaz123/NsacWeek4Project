afc151c964c0779922cc0f9edcae5ff1bffe2d35
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-13 13:34:50 +0100

    AA: added to entities class

--- src/main/java/com/qa/main/entities/ElectricVehicle.java
+++ src/main/java/com/qa/main/entities/ElectricVehicle.java
@@ -1,5 +1,35 @@
 package com.qa.main.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class ElectricVehicle {
+
+	@Entity
+	public class ElectricVehicle {
+		
+
+		@Id 
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+
+		@Column(nullable=false)
+		private String carMake;
+		
+		@Column(nullable=false)
+		private String carModel;
+		
+		@Column(nullable=false)
+		private int releaseYear;
+		
+		@Column(nullable=false)
+		private int mileRange;
+	
+}
 
 }
