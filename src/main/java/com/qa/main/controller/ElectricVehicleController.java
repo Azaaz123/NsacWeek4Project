d153ef822c52e2ded2f768082c5629ed6ce54ba2
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-13 16:39:29 +0100

    AA:created temporay storage for array list

--- src/main/java/com/qa/main/controller/ElectricVehicleController.java
+++ src/main/java/com/qa/main/controller/ElectricVehicleController.java
@@ -1,10 +1,18 @@
 package com.qa.main.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.main.entities.ElectricVehicle;
 
 @RestController
 public class ElectricVehicleController {
 	
+	/// Temporary storage
+	
+	private List<ElectricVehicle> cars = new ArrayList <>();
 	
 	
 
