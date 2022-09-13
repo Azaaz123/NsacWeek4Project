c5f9d7cdec33c38193d7a5cd8e97dd47a0c4b3fc
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-13 13:51:39 +0100

    AA: genereated getters and setters with id

--- src/main/java/com/qa/main/entities/ElectricVehicle.java
+++ src/main/java/com/qa/main/entities/ElectricVehicle.java
@@ -1,17 +1,14 @@
 package com.qa.main.entities;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class ElectricVehicle {
 
-	@Entity
-	public class ElectricVehicle {
 		
 
 		@Id 
@@ -29,7 +26,48 @@ public class ElectricVehicle {
 		
 		@Column(nullable=false)
 		private int mileRange;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getCarMake() {
+			return carMake;
+		}
+
+		public void setCarMake(String carMake) {
+			this.carMake = carMake;
+		}
+
+		public String getCarModel() {
+			return carModel;
+		}
+
+		public void setCarModel(String carModel) {
+			this.carModel = carModel;
+		}
+
+		public int getReleaseYear() {
+			return releaseYear;
+		}
+
+		public void setReleaseYear(int releaseYear) {
+			this.releaseYear = releaseYear;
+		}
+
+		public int getMileRange() {
+			return mileRange;
+		}
+
+		public void setMileRange(int mileRange) {
+			this.mileRange = mileRange;
+		}
 	
+		
+		
 }
 
-}
