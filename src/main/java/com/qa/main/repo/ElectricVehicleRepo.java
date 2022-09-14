b6b27a248656577d29a71f4b5cdaf37074bbc73c
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-14 11:06:57 +0100

    AA:JPA repository added

--- src/main/java/com/qa/main/repo/ElectricVehicleRepo.java
+++ src/main/java/com/qa/main/repo/ElectricVehicleRepo.java
@@ -1,8 +1,15 @@
 package com.qa.main.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ElectricVehicleRepo {
+import com.qa.main.entities.ElectricVehicle;
 
+@Repository
+public class ElectricVehicleRepo extends JpaRepository<ElectricVehicle, Long> {
+
+	
+	
 }
