76932a9156b1073e15ab85133cdb43e21856791d
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-14 10:33:57 +0100

    AA:added delete request

--- src/main/java/com/qa/main/controller/ElectricVehicleController.java
+++ src/main/java/com/qa/main/controller/ElectricVehicleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,8 +51,12 @@ public class ElectricVehicleController {
 			 return cars.get(id);
 		 }
 		 
-		 
-		 
+	
+			// Delete Requests (DELETE)
+		 @DeleteMapping("/delete/{id}")
+		 public ElectricVehicle delete(@PathVariable int id) {
+			return this.cars.remove(id);
+		 }
 		 
 		 
 }
