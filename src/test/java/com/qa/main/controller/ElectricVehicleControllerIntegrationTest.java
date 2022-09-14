2a87a22d00438775384768966c8288875f1a3372
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-14 16:18:24 +0100

    AA: outlined test to be added

--- src/test/java/com/qa/main/controller/ElectricVehicleControllerIntegrationTest.java
+++ src/test/java/com/qa/main/controller/ElectricVehicleControllerIntegrationTest.java
@@ -1,8 +1,14 @@
 package com.qa.main.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,4 +54,81 @@ public class ElectricVehicleControllerIntegrationTest {
 				
 		}
 		
+	@Test
+	public void updateTest() throws Exception {
+//		     An Object for sending in the body in the request 		
+				ElectricVehicle input = new ElectricVehicle("Testla", "S", 2020, "awd",  77, 350);
+				String inputAsJSON = mapper.writeValueAsString(input); 
+
+				// An object for checking response
+				 ElectricVehicle response = new ElectricVehicle(1L,"Testla", "S", 2020, "awd",  77, 350);
+					String responseAsJSON = mapper.writeValueAsString(response); 
+				
+				 
+				 mvc.perform(put("/update/1")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(inputAsJSON))
+						.andExpect(status().isOk())
+						.andExpect(content().json(responseAsJSON));
+					
+			}
+	
+	
+	
+	@Test
+	public void getAllTest() throws Exception  {
+		//Created a List
+			List<ElectricVehicle> result = new ArrayList<>();
+	// Added my expected ElectricVehicle to the list
+			result.add(new ElectricVehicle(1L,"Kia", "EV6", 2022, "awd",  77, 313));
+			
+			String resultAsJSON = mapper.writeValueAsString(result); 
+			
+			mvc.perform(get("/getAll")
+					.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().json(resultAsJSON));
+					
+	}
+	
+	@Test
+	public void getByIdTest() throws Exception {
+		ElectricVehicle result = new ElectricVehicle(1L,"Kia", "EV6", 2022, "awd",  77, 313);
+		
+		String resultAsJSON = mapper.writeValueAsString(result); 
+		
+		mvc.perform(get("/getById/1")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(resultAsJSON));
+	}
+	
+	@Test
+	public void getByCarMakeTest() {
+	}
+	
+	@Test
+	public void getByCarModeltest() {
+	}
+	
+	
+	
+	@Test
+	public void getByReleaseYear() {
+	}
+	
+	
+	@Test
+	public void getByMileRangeGreaterThan(){
+	}
+	
+	
+	@Test
+	public void deleteTet(){
+	}
+	
+	
+
 }
+	
+
