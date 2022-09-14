48ccb8534a74f9413229e4a282efecae6475aa24
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-14 16:45:58 +0100

    AA: mockbean, mapper and MVC added

--- src/test/java/com/qa/main/controller/ElectricVehicleControllerUnitTest.java
+++ src/test/java/com/qa/main/controller/ElectricVehicleControllerUnitTest.java
@@ -1,8 +1,26 @@
 package com.qa.main.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.main.service.ElectricVehicleService;
 
 @WebMvcTest
 public class ElectricVehicleControllerUnitTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper; 
+	
+	@MockBean
+	private ElectricVehicleService service;
+	
+	@Test
+	public void createTest() {}
 }
