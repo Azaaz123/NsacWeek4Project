f8f226bf951cefcc21d082daa913c3654d807370
Azaaz123 <112072120+Azaaz123@users.noreply.github.com>
2022-09-14 16:13:26 +0100

    AA: integration test imports added

--- src/test/java/com/qa/main/controller/ElectricVehicleControllerIntegrationTest.java
+++ src/test/java/com/qa/main/controller/ElectricVehicleControllerIntegrationTest.java
@@ -1,5 +1,17 @@
 package com.qa.main.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+@SpringBootTest
+@AutoConfigureMockMvc // Allows us to send mock request using MockMvc
+@Sql(scripts = {"classpath:testschema.sql","classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test") // switch to test profile (H2 database)
 public class ElectricVehicleControllerIntegrationTest {
 
+	
+	
 }
